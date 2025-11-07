33e59590cfadb941638a9ad8c2c4396ac2f7f1d4
meri061203 <merrymey987@gmail.com>
2025-11-06 18:36:40 -1200

    feat : add web

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Rap2hpoutre\LaravelLogViewer\LogViewerController;
 Route::get('/', [PortalController::class, 'login'])->name('index');
 Route::post('/login', [PortalController::class, 'logindb'])->name('logindb');
 Route::post('log-error', [PortalController::class, 'error'])->name('log-error');
+Route::get('/logout', [PortalController::class, 'logout'])->name('logout');
 
 
 
