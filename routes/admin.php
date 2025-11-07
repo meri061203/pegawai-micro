d1b160ab89311df98b30016c459bdb72500a03c2
meri061203 <merrymey987@gmail.com>
2025-11-06 14:04:12 -1200

    feat : add route

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\Person\PersonController;
+use App\Http\Controllers\Admin\Sdm\PersonSdmController;
 use App\Http\Controllers\Content\PortalController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,19 @@ Route::get('view-file/{folder}/{filename}', [PortalController::class, 'viewFile'
     Route::post('update/{id}', [PersonController::class, 'update'])
         ->name('person.update');
 });
+
+Route::prefix('sdm')->group(function () {
+    Route::get('/', [PersonSdmController::class, 'index'])
+        ->name('sdm.sdm.index');
+    Route::get('data', [PersonSdmController::class, 'list'])
+        ->name('sdm.sdm.list');
+    Route::get('show/{id}', [PersonSdmController::class, 'show'])
+        ->name('sdm.sdm.show');
+    Route::post('/store', [PersonSdmController::class, 'store'])
+        ->name('sdm.sdm.store');
+    Route::post('update/{id}', [PersonSdmController::class, 'update'])
+        ->name('sdm.sdm.update');
+    Route::get('histori/{id}', [PersonSdmController::class, 'histori'])
+        ->name('sdm.sdm.histori');
+    Route::get('find/by/nik/{id}', [PersonSdmController::class, 'find_by_nik'])
+        ->name('sdm.sdm.find_by_nik'); });
